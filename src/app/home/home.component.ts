e550beb31d7b8048107e9455b4425cc121eceaf3
lola Seoane <lola.seoane@advnz.de>
2023-03-14 12:51:50 +0100

    Ejercicio1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,43 @@
 import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
+
+interface service {
+  name: string;
+  price: number;
+  selected: boolean
+}
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  
-    option1 =false;
-  
-  
-    option2 = false;
-
-  
-    option3 = false;
-  
+  totalSum:number = 0
+  optionSelected:boolean = true
   
 
-}
+  services: service[] = [
+    { name: 'Una página web', price: 500, selected: false },
+    { name: 'Una cosultoría SEO', price: 300, selected: false },
+    { name: 'Una campaña de Google Ads', price: 200, selected: false },
+  ];
+  
+  
+  sumTotalPrices() {
+    this.totalSum = 0;
+      this.services.forEach(service => {
+        if (service.selected) {
+          this.totalSum += service.price;
+        }
+      });
+    }
+    
+  }
+
+
+
+
+
+
+
