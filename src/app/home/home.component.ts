f499e3745b604930be6a460b4a33bae5b9b57857
lola Seoane <lola.seoane@advnz.de>
2023-03-22 02:11:39 +0100

    Refactor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,62 +1,7 @@
 
-
-/*import { Component,OnInit } from '@angular/core';
-import { PanelService } from '../services/panel.service';
-import { Subject } from 'rxjs';
-import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
-
-interface service {
-  name: string;
-  price: number;
-  selected: boolean;
-}
-
-@Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
-})
-export class HomeComponent {
-  myForm!: FormGroup;
-  totalSum: number = 0;
-  totalpanel = this.panelService.totalp;
-  totalpSubject: Subject<number> = new Subject<number>();
-
-  constructor(private panelService: PanelService, private fb: FormBuilder) {
-        this.sumTotalPrices();
-    
-  }
-  ngOnInit() {
-    this.myForm = this.fb.group({});
-  }
- 
-  services: service[] = [
-    { name: 'Una página web', price: 500, selected: false },
-    { name: 'Una consultoría SEO', price: 300, selected: false },
-    { name: 'Una campaña de Google Ads', price: 200, selected: false },
-  ];
-
-  
-  sumTotalPrices() {
-    this.totalSum = 0;
-    this.totalpanel = this.panelService.totalp;
-    console.log(this.totalpanel);
-    this.services.forEach((service, index) => {
-      if (service.selected) {
-        if (index === 0) {
-          this.totalSum = service.price + this.totalpanel;
-          
-        } else {
-          this.totalSum += service.price;
-        }
-      }
-    });
-  }
-    
-  }*/
-  import { Component,OnInit } from '@angular/core';
+  import { Component } from '@angular/core';
   import { PanelService } from '../services/panel.service';
-  import { FormBuilder, FormGroup, FormArray,FormControl } from '@angular/forms';
+  import { FormBuilder, FormGroup,FormControl } from '@angular/forms';
   
   interface service {
     name: string;
--- src/app/panell/panell.component.ts
+++ src/app/panell/panell.component.ts
@@ -1,6 +1,6 @@
 import { PanelService } from './../services/panel.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
+import { Component,} from '@angular/core';
 
 
 
@@ -9,38 +9,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './panell.component.html',
   styleUrls: ['./panell.component.css']
 })
-
-/*export class PanellComponent implements OnInit {
-  pagesNumber: number = 1;
-  languageNumber: number = 1;
-  form!: FormGroup;
-
-  constructor(private fb: FormBuilder, private tp: PanelService) { }
-  ngOnInit() {
-    this.form = this.fb.group({
-      pages: [1, Validators.required],
-      languages: [1, Validators.required]
-    });
-  }
-
-  obtainN(): void {
-    const pagesN = <HTMLInputElement>document.getElementById("inputPagNumber")
-    if (pagesN) {
-      this.pagesNumber = parseInt(pagesN.value);
-
-    }
-    const langN = <HTMLInputElement>document.getElementById("inputLanguagesNumber")
-    if (langN) {
-      this.languageNumber = parseInt(langN.value);
-
-    }
-
-    if (this.pagesNumber > 0 || this.languageNumber > 0) {
-      const total = this.tp.calculate(this.pagesNumber, this.languageNumber);
-
-    }
-  }
-}*/
 
 export class PanellComponent {
   form: FormGroup;
